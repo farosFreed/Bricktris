a80d6d139efeae573fdb6e6023f65fda6206faa9
farosFreed <jessica.divers@gmail.com>
2024-01-31 17:34:39 -0700

    build files

---
+++ .output/server/chunks/app/_nuxt/island-renderer-YCXLir6_.mjs
@@ -0,0 +1,48 @@
+import { defineComponent, onErrorCaptured, createVNode } from 'vue';
+import { c as createError } from '../server.mjs';
+import '../../nitro/node-server.mjs';
+import 'node:http';
+import 'node:https';
+import 'fs';
+import 'path';
+import 'node:fs';
+import 'node:url';
+import 'unhead';
+import '@unhead/shared';
+import 'ts-invariant';
+import 'zen-observable-ts';
+import '@wry/caches';
+import 'graphql';
+import 'optimism';
+import '@wry/equality';
+import '@wry/trie';
+import 'vue/server-renderer';
+import '@iconify/vue/dist/offline';
+import '@iconify/vue';
+import 'graphql-tag';
+
+const islandComponents = {};
+const islandRenderer = defineComponent({
+  props: {
+    context: {
+      type: Object,
+      required: true
+    }
+  },
+  setup(props) {
+    const component = islandComponents[props.context.name];
+    if (!component) {
+      throw createError({
+        statusCode: 404,
+        statusMessage: `Island component not found: ${props.context.name}`
+      });
+    }
+    onErrorCaptured((e) => {
+      console.log(e);
+    });
+    return () => createVNode(component || "span", { ...props.context.props, "data-island-uid": "" });
+  }
+});
+
+export { islandRenderer as default };
+//# sourceMappingURL=island-renderer-YCXLir6_.mjs.map
---
+++ .output/server/chunks/app/client.manifest.mjs
@@ -0,0 +1,97 @@
+const client_manifest = {
+  "_vue.f36acd1f.-tbkfR32.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "vue.f36acd1f.-tbkfR32.js",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "css": [],
+    "file": "error-404.NAztsHLa.js",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js",
+      "_vue.f36acd1f.-tbkfR32.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue"
+  },
+  "error-404.qFGwA4uS.css": {
+    "file": "error-404.qFGwA4uS.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "css": [],
+    "file": "error-500.y65m3bKi.js",
+    "imports": [
+      "_vue.f36acd1f.-tbkfR32.js",
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue"
+  },
+  "error-500.V0P2JAtD.css": {
+    "file": "error-500.V0P2JAtD.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "node_modules/nuxt-icon/dist/runtime/IconCSS.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "css": [],
+    "file": "IconCSS.y8i2W_r9.js",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "node_modules/nuxt-icon/dist/runtime/IconCSS.vue"
+  },
+  "IconCSS.GdgQB7f8.css": {
+    "file": "IconCSS.GdgQB7f8.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "node_modules/nuxt/dist/app/entry.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "css": [
+      "entry.tmEo-gHM.css"
+    ],
+    "dynamicImports": [
+      "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue",
+      "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue"
+    ],
+    "file": "entry.5CuazK1A.js",
+    "isEntry": true,
+    "src": "node_modules/nuxt/dist/app/entry.js",
+    "_globalCSS": true
+  },
+  "entry.tmEo-gHM.css": {
+    "file": "entry.tmEo-gHM.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  }
+};
+
+export { client_manifest as default };
+//# sourceMappingURL=client.manifest.mjs.map
