e1e716e311d7d7e59a60b5a6878afde792727f31
farosFreed <jessica.divers@gmail.com>
2024-01-31 17:55:56 -0700

    fix image paths

--- components/Bricklist.vue
+++ components/Bricklist.vue
@@ -78,38 +78,38 @@ ul {
   height: 24px;
 }
 .tetromino.Z {
-  background: url(images/Z.svg) no-repeat center center;
+  background: url(/images/Z.svg) no-repeat center center;
   background-size: contain;
   filter: $Z-tetromino-filter;
 }
 .tetromino.I {
   height: 36px;
-  background: url(images/I.svg) no-repeat center center;
+  background: url(/images/I.svg) no-repeat center center;
   background-size: contain;
   filter: $I-tetromino-filter;
 }
 .tetromino.L {
-  background: url(images/L.svg) no-repeat center center;
+  background: url(/images/L.svg) no-repeat center center;
   background-size: contain;
   filter: $L-tetromino-filter;
 }
 .tetromino.O {
-  background: url(images/O.svg) no-repeat center center;
+  background: url(/images/O.svg) no-repeat center center;
   background-size: contain;
   filter: $O-tetromino-filter;
 }
 .tetromino.T {
-  background: url(images/T.svg) no-repeat center center;
+  background: url(/images/T.svg) no-repeat center center;
   background-size: contain;
   filter: $T-tetromino-filter;
 }
 .tetromino.S {
-  background: url(images/S.svg) no-repeat center center;
+  background: url(/images/S.svg) no-repeat center center;
   background-size: contain;
   filter: $S-tetromino-filter;
 }
 .tetromino.J {
-  background: url(images/J.svg) no-repeat center center;
+  background: url(/images/J.svg) no-repeat center center;
   background-size: contain;
   filter: $J-tetromino-filter;
 }
